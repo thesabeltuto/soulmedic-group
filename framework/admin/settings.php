9102ba3ea65cb91bbbde6b3eeada57b01568d83d
Thesabel Tuto <thesabeltuto@gmail.com>
2016-12-23 04:58:02 +0800

    1.0.4
    1.0.4

--- framework/admin/settings.php
+++ framework/admin/settings.php
@@ -1,13 +1,13 @@
 <?php
-add_action('admin_menu', 'create_admin_menu_kris');
-add_action('init', 'soulmedic_kris_css');
+add_action('admin_menu', 'create_admin_menu_group');
+add_action('init', 'soulmedic_group_css');
 
-function create_admin_menu_kris() {
-	add_menu_page ('Theme Support', 'Theme Support','manage_theme','soulmedic_kris_support','soulmedic_kris_support', '', 27); //after soulmedic-wm4d menu
+function create_admin_menu_group() {
+	add_menu_page ('Theme Support', 'Theme Support','manage_theme','soulmedic_group_support','soulmedic_group_support', '', 27); //after soulmedic-wm4d menu
 }
 
-function soulmedic_kris_css() {
-	wp_register_style('soulmedic_kris_css.css', THEME_URL.'/framework/admin/admin.css', '', $GLOBALS['THEME_CSS_VERSION'], '');
-	wp_enqueue_style('soulmedic_kris_css.css');
+function soulmedic_group_css() {
+	wp_register_style('soulmedic_group_css.css', THEME_CHILD_URL.'/framework/admin/admin.css', '', $GLOBALS['THEME_CSS_VERSION'], '');
+	wp_enqueue_style('soulmedic_group_css.css');
 }
 ?>
