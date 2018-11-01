7f5fcc28c8b9e2f227dedca3d57cd4f841032150
Thesabel Tuto <thesabeltuto@gmail.com>
2018-11-02 04:52:46 +0800

    1.0.6

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@ define('THEME_CHILD_DIR', get_template_directory().$THEME_CHILD);
 define('THEME_CHILD_URL', get_template_directory_uri().$THEME_CHILD);
 
 // Global variables
-$THEME_VERSION = '1.0.5';
-$THEME_CSS_VERSION = '1.0.1';
+$THEME_VERSION = '1.0.6';
+$THEME_CSS_VERSION = '1.0.2';
 
 require_once(THEME_CHILD_DIR.'/framework/admin.php');
 require_once(THEME_CHILD_DIR.'/framework/sidebars.php');
